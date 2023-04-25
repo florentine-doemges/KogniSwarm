e3a5abc0308b1ae42925d87f9b1ec5a702687a3a
Florentine <florentine@doemges.net>
2023-04-25 19:17:16 +0200

    updated README.md

--- src/main/kotlin/net/doemges/kogniswarm/thinking/ThoughtProcess.kt
+++
@@ -1,9 +0,0 @@
-package net.doemges.kogniswarm.thinking
-
-class ThoughtProcess(
-    val thoughts: String,
-    val reasoning: String,
-    val plan: List<String>,
-    val criticism: String,
-    val nextAction: String = plan.first()
-)

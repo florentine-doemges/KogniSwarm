3a551816d39ee6a3700e90f9f15dc7dca87d2e55
Florentine <florentine@doemges.net>
2023-05-17 07:34:19 +0200

    improved command descriptions

--- src/main/kotlin/net/doemges/kogniswarm/command/Command.kt
+++ src/main/kotlin/net/doemges/kogniswarm/command/Command.kt
@@ -25,7 +25,7 @@ abstract class BaseCommand(
 @Component
 class AnalyzeCodeCommand : BaseCommand(
     name = "analyzeCode",
-    description = "A function accepting a string (code), returning a create chat completion API response and code improvement suggestions.",
+    description = "Analyzes and suggests improvements for the provided code. Useful for code optimization.",
     args = mapOf("code" to "The code string for evaluation.")
 ) {
     override fun execute(args: Map<String, List<String>>): String {
@@ -36,8 +36,8 @@ class AnalyzeCodeCommand : BaseCommand(
 @Component
 class ExecuteCodeCommand : BaseCommand(
     name = "executeCode",
-    description = "A function accepting a string (code) and executing it.",
-    args = mapOf("code" to "The name of the file to execute")
+    description = "Executes the provided code snippet. Useful for dynamic code execution.",
+    args = mapOf("code" to "File name to execute.")
 ) {
     override fun execute(args: Map<String, List<String>>): String {
         return "Not implemented yet."
@@ -47,8 +47,11 @@ class ExecuteCodeCommand : BaseCommand(
 @Component
 class ImproveCodeCommand : BaseCommand(
     name = "improveCode",
-    description = "A function that takes in code and suggestions and returns improved code.",
-    args = mapOf("code" to "The code string for evaluation.", "suggestions" to "The suggestions for improvement.")
+    description = "Improves code based on provided suggestions. Useful for code enhancement.",
+    args = mapOf(
+        "code" to "Code to improve.",
+        "suggestions" to "Improvement suggestions."
+    )
 ) {
     override fun execute(args: Map<String, List<String>>): String {
         return "Not implemented yet."
@@ -58,8 +61,8 @@ class ImproveCodeCommand : BaseCommand(
 @Component
 class WriteTestCommand : BaseCommand(
     name = "writeTest",
-    description = "A function accepting a string (code), returning a create chat completion API response and code improvement suggestions.",
-    args = mapOf("code" to "The code string for evaluation.")
+    description = "Generates testable Kotlin code for the provided code. Useful for code testing automation.",
+    args = mapOf("code" to "Code to test.")
 ) {
     override fun execute(args: Map<String, List<String>>): String {
         return "Not implemented yet."
@@ -69,8 +72,8 @@ class WriteTestCommand : BaseCommand(
 @Component
 class SummarizeTextCommand : BaseCommand(
     name = "summarizeText",
-    description = "A function accepting a string (text), returning a summary of the text.",
-    args = mapOf("text" to "The text to summarize.")
+    description = "Creates a summary of the provided text. Useful for condensing lengthy text.",
+    args = mapOf("text" to "Text to summarize.")
 ) {
     override fun execute(args: Map<String, List<String>>): String {
         return "Not implemented yet."
@@ -80,11 +83,12 @@ class SummarizeTextCommand : BaseCommand(
 @Component
 class WriteToFileCommand : BaseCommand(
     name = "writeToFile",
-    description = "A function accepting a string (text) and a string (filename), writing the text to the file.",
+    description = "Writes the provided text to a specified file. Useful for data archiving.",
     args = mapOf(
-        "text" to "The text to write.",
-        "filename" to "The name of the file to write to."
+        "text" to "Text to write.",
+        "filename" to "Target file."
     )
+
 ) {
     override fun execute(args: Map<String, List<String>>): String {
         return "Not implemented yet."
@@ -94,8 +98,8 @@ class WriteToFileCommand : BaseCommand(
 @Component
 class ReadFromFileCommand : BaseCommand(
     name = "readFromFile",
-    description = "A function accepting a string (filename), returning the content of the file.",
-    args = mapOf("filename" to "The name of the file to read from.")
+    description = "Reads and returns the content of a specified file. Useful for data retrieval.",
+    args = mapOf("filename" to "File to read.")
 ) {
     override fun execute(args: Map<String, List<String>>): String {
         return "Not implemented yet."
@@ -105,8 +109,12 @@ class ReadFromFileCommand : BaseCommand(
 @Component
 class CloneGitRepositoryCommand : BaseCommand(
     name = "cloneGitRepository",
-    description = "A function accepting a string (url) and a string (path), cloning the git repository to the path.",
-    args = mapOf("url" to "The url of the git repository to clone.", "path" to "The path to clone the repository to.")
+    description = "Clones a Git repository from a URL to a specified path. Useful for project setup.",
+    args = mapOf(
+        "url" to "Repository URL.",
+        "path" to "Target path."
+    )
+
 ) {
     override fun execute(args: Map<String, List<String>>): String {
         return "Not implemented yet."
@@ -116,8 +124,8 @@ class CloneGitRepositoryCommand : BaseCommand(
 @Component
 class GoogleSearchCommand : BaseCommand(
     name = "googleSearch",
-    description = "A function accepting a string (query), returning a list of search results.",
-    args = mapOf("query" to "The query to search for.")
+    description = "Performs a Google search and returns results. Useful for web-based research.",
+    args = mapOf("query" to "Search query.")
 ) {
     override fun execute(args: Map<String, List<String>>): String {
         return "Not implemented yet."
@@ -127,8 +135,8 @@ class GoogleSearchCommand : BaseCommand(
 @Component
 class SendTweetCommand : BaseCommand(
     name = "sendTweet",
-    description = "A function accepting a string (tweet), sending the tweet.",
-    args = mapOf("tweet" to "The tweet to send.")
+    description = "Sends a tweet. Useful for social media updates.",
+    args = mapOf("tweet" to "Tweet content.")
 ) {
     override fun execute(args: Map<String, List<String>>): String {
         return "Not implemented yet."
@@ -138,8 +146,8 @@ class SendTweetCommand : BaseCommand(
 @Component
 class DownloadFileCommand : BaseCommand(
     name = "downloadFile",
-    description = "A function accepting a string (url) and a string (path), downloading the file to the path.",
-    args = mapOf("url" to "The url of the file to download.", "path" to "The path to download the file to.")
+    description = "Downloads a file from a URL to a specified path. Useful for data acquisition.",
+    args = mapOf("url" to "File URL.", "path" to "Target path.")
 ) {
     override fun execute(args: Map<String, List<String>>): String {
         return "Not implemented yet."
@@ -149,8 +157,8 @@ class DownloadFileCommand : BaseCommand(
 @Component
 class ScrapeTextCommand : BaseCommand(
     name = "scrapeText",
-    description = "A function accepting a string (url), returning the text of the website.",
-    args = mapOf("url" to "The url of the website to scrape.")
+    description = "Extracts and returns the text content of a website. Useful for web content extraction.",
+    args = mapOf("url" to "Website URL.")
 ) {
     override fun execute(args: Map<String, List<String>>): String {
         return "Not implemented yet."
@@ -160,8 +168,8 @@ class ScrapeTextCommand : BaseCommand(
 @Component
 class ScrapeLinksCommand : BaseCommand(
     name = "scrapeLinks",
-    description = "A function accepting a string (url), returning a list of links on the website.",
-    args = mapOf("url" to "The url of the website to scrape.")
+    description = "Extracts and returns all links from a website. Useful for website crawling.",
+    args = mapOf("url" to "Website URL.")
 ) {
     override fun execute(args: Map<String, List<String>>): String {
         return "Not implemented yet."

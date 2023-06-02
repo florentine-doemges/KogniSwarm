8331bbc0dffc40e28036e3250d21dbdb289c436c
Florentine <florentine@doemges.net>
2023-06-02 16:10:09 +0200

    some small inital mods on the ui template

--- vue-element-admin/src/views/dashboard/index.vue
+++ vue-element-admin/src/views/dashboard/index.vue
@@ -1,31 +1,20 @@
 <template>
-  <div class="dashboard-container">
-    <component :is="currentRole" />
+  <div className="dashboard-container">
+    <admin-dashboard/>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import {mapGetters} from 'vuex'
 import adminDashboard from './admin'
-import editorDashboard from './editor'
 
 export default {
   name: 'Dashboard',
-  components: { adminDashboard, editorDashboard },
-  data() {
-    return {
-      currentRole: 'adminDashboard'
-    }
-  },
+  components: {adminDashboard},
   computed: {
     ...mapGetters([
       'roles'
     ])
-  },
-  created() {
-    if (!this.roles.includes('admin')) {
-      this.currentRole = 'editorDashboard'
-    }
   }
 }
 </script>
